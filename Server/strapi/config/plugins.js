e5b010f6512d9de694ea9ce27981ddb817941bb2
Nadeera Gayantha Punchihewa <nadeera.gayantha@paraqum.com>
2023-12-01 14:55:52 +0530

    new pugin added

--- Server/strapi/config/plugins.js
+++ Server/strapi/config/plugins.js
@@ -1,3 +1,4 @@
+
 module.exports = ({ env }) => ({
     // ...
     email: {
@@ -18,5 +19,47 @@ module.exports = ({ env }) => ({
       },
     },
     // ...
+
+
+      // ...
+  "image-optimizer": {
+    enabled: true,
+    config: {
+      include: ["jpeg", "jpg", "png"],
+      exclude: ["gif"],
+      formats: [ "webp"],
+      sizes: [
+        {
+          name: "xs",
+          width: 300,
+        },
+        {
+          name: "sm",
+          width: 768,
+        },
+        {
+          name: "md",
+          width: 1280,
+        },
+        {
+          name: "lg",
+          width: 1920,
+        },
+        {
+          name: "xl",
+          width: 2840,
+          withoutEnlargement: true, // Won't create an image larger than the original size
+        },
+        {
+          name: "original", // Uses the original size but still transforms for formats
+        },
+      ],
+      additionalResolutions: [1.5, 3],
+      quality: 70,
+    },
+  } 
+  // ...
+
+
   });
   
